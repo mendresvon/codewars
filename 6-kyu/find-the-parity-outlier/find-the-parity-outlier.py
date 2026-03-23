45b02763aaef10d6178a9c57c0c1e2c07713ee4a
Von Breznev Antido Mendres <mendresvon@gmail.com>
2026-03-23 23:07:34 +0800

    Update solution - CodeHub

--- 6-kyu/find-the-parity-outlier/find-the-parity-outlier.py
+++ 6-kyu/find-the-parity-outlier/find-the-parity-outlier.py
@@ -1,18 +1,11 @@
 def find_outlier(integers):
-    odd_count = 0
-    even_count = 0
-    for i in range(3):
-        if integers[i] % 2 == 0:
-            even_count += 1
-        else:
-            odd_count += 1
+    odds = []
+    evens = []
     
-    outlier_remainder = 0
-    if even_count > odd_count:
-        outlier_remainder = 1
-    else:
-        outlier_remainder = 0
-        
     for num in integers:
-        if num % 2 == outlier_remainder:
-            return num
+        if num % 2:
+            odds.append(num)
+        else:
+            evens.append(num)
+    
+    return odds[0] if len(odds) < len(evens) else evens[0]
