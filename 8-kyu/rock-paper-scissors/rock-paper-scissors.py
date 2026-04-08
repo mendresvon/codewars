56462cb29be28c136818d9a9df7fbe8c1b5d989d
Von Breznev Antido Mendres <mendresvon@gmail.com>
2026-04-08 09:28:19 +0800

    Update solution - CodeHub

--- 8-kyu/rock-paper-scissors/rock-paper-scissors.py
+++ 8-kyu/rock-paper-scissors/rock-paper-scissors.py
@@ -1,19 +1,13 @@
 def rps(p1, p2):
-    #your code here
-    if p1 == "rock" and p2 == "scissors":
-        return "Player 1 won!"
-    elif p2 == "rock" and p1 == "scissors":
-        return "Player 2 won!"
+    beats = {
+        "rock": "scissors",
+        "scissors": "paper",
+        "paper": "rock",
+            }
     
-    elif p1 == "scissors" and p2 == "paper":
+    if beats[p1] == p2:
         return "Player 1 won!"
-    elif p2 == "scissors" and p1 == "paper":
+    elif beats[p2] == p1:
         return "Player 2 won!"
-    
-    elif p1 == "paper" and p2 == "rock":
-        return "Player 1 won!"
-    elif p1 == "rock" and p2 == "paper":
-        return "Player 2 won!"
-​
     else:
         return "Draw!"
