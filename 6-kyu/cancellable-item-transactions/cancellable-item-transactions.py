e70494d74e9bc2bf91c83951fa49f013870cb328
Von Breznev Antido Mendres <mendresvon@gmail.com>
2026-04-09 14:42:52 +0800

    Update solution - CodeHub

--- 6-kyu/cancellable-item-transactions/cancellable-item-transactions.py
+++ 6-kyu/cancellable-item-transactions/cancellable-item-transactions.py
@@ -3,38 +3,34 @@ def calculate(price_dict, transaction):
     history = defaultdict(list)
     total = 0
     i = 0
+    n = len(transaction)
     
-    while i < len(transaction):
+    while i < n:
         char = transaction[i]
         
-        # cancel case
-        if char.isalpha():
+        if char.isalpha(): # cancel last transaction
             if char in history and history[char]:
                 to_remove = history[char].pop()
                 total -= to_remove
-            i += 1
-            continue
-                
-        else: # not cancel case
+​
+        else: # add transaction
             sign = 1
-            # if char is '-' sign, set sign to negative and move i forward
             if char == '-':
                 sign = -1
                 i += 1
             
-            # keep moving until we arrive at a letter
             j = i
-            while i < len(transaction) and transaction[i].isdigit():
+            while i < n and transaction[i].isdigit():
                 i += 1
-            
-            if i >= len(transaction):
+            if i >= n:
                 break
             
-            curr_char = transaction[i]
             qty = int(transaction[j:i])
-            curr_total = qty * sign * price_dict[curr_char]
-            history[curr_char].append(curr_total)
+            curr_total = qty * sign * price_dict[transaction[i]]
             total += curr_total
-            i += 1
+            
+            history[transaction[i]].append(curr_total)
+        
+        i += 1
         
     return total
