244366c86e34fcbd7731e6ea7aa233d5c9c90d89
Von Breznev Antido Mendres <mendresvon@gmail.com>
2026-04-22 09:06:58 +0800

    Update solution - CodeHub

--- 7-kyu/find-the-next-perfect-square/find-the-next-perfect-square.py
+++ 7-kyu/find-the-next-perfect-square/find-the-next-perfect-square.py
@@ -1,7 +1,4 @@
 def find_next_square(sq):
-    # Return the next square if sq is a square, -1 otherwise
-    square_root = sq ** (1/2)
-    if square_root % 1 != 0:
-        return -1
-    else:
-        return (square_root + 1) ** 2
+    if sq ** 0.5 % 1 == 0:
+        return (sq ** 0.5 + 1) ** 2
+    return -1
