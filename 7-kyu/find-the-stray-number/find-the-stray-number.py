2d390cada50c8755508e005cc39b33059004065a
Von Breznev Antido Mendres <mendresvon@gmail.com>
2026-05-25 10:45:37 +0800

    Update solution - CodeHub

--- 7-kyu/find-the-stray-number/find-the-stray-number.py
+++ 7-kyu/find-the-stray-number/find-the-stray-number.py
@@ -1,6 +1,7 @@
-from collections import Counter
 def stray(arr):
-    count = Counter(arr)
-    for key,val in count.items():
-        if val == 1:
-            return key
+    if arr[0] != arr[1] and arr[0] != arr[2]:
+        return arr[0]
+    
+    for x in arr:
+        if x != arr[0]:
+            return x
