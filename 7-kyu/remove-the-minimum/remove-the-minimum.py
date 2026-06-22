fd346315d3a1f461d566eb1fddfb57b4208ba64f
Von Breznev Antido Mendres <mendresvon@gmail.com>
2026-06-22 11:58:15 +0800

    Update solution - CodeHub

--- 7-kyu/remove-the-minimum/remove-the-minimum.py
+++ 7-kyu/remove-the-minimum/remove-the-minimum.py
@@ -1,10 +1,12 @@
 def remove_smallest(numbers):
-    if not numbers: return []
-    low = min(numbers)
+    n = len(numbers)
+    if n < 1:
+        return numbers
     
+    to_remove = min(numbers)
     res = []
-    for i in range(len(numbers)):
-        if numbers[i] == low:
+    for i in range(n):
+        if numbers[i] == to_remove:
             res.extend(numbers[i+1:])
             break
         res.append(numbers[i])
