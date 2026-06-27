04aca5d60b36b857153ce64fe840591ba07e9aba
Von Breznev Antido Mendres <mendresvon@gmail.com>
2026-06-27 10:12:02 +0800

    Add solution - CodeHub

---
+++ 5-kyu/calculating-with-functions/calculating-with-functions.py
@@ -0,0 +1,15 @@
+def zero(op = None): return 0 if op is None else op(0)
+def one(op = None): return 1 if op is None else op(1)
+def two(op = None): return 2 if op is None else op(2)
+def three(op = None): return 3 if op is None else op(3)
+def four(op = None): return 4 if op is None else op(4)
+def five(op = None): return 5 if op is None else op(5)
+def six(op = None): return 6 if op is None else op(6)
+def seven(op = None): return 7 if op is None else op(7)
+def eight(op = None): return 8 if op is None else op(8)
+def nine(op = None): return 9 if op is None else op(9)
+​
+def plus(right): return lambda left: left + right
+def minus(right): return lambda left: left - right
+def times(right): return lambda left: left * right
+def divided_by(right): return lambda left: left // right
