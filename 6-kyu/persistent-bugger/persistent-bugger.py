76fe3d0cd448b38b001fd5e014878d6ea10f1dc6
Von Breznev Antido Mendres <mendresvon@gmail.com>
2026-06-30 10:31:40 +0800

    Update solution - CodeHub

--- 6-kyu/persistent-bugger/persistent-bugger.py
+++ 6-kyu/persistent-bugger/persistent-bugger.py
@@ -1,12 +1,12 @@
-from math import prod
-​
 def persistence(n):
     digits = [int(d) for d in str(n)]
     count = 0
     
     while len(digits) > 1:
-        product = prod(digits)
+        product = 1
+        for d in digits:
+            product *= d
         digits = [int(d) for d in str(product)]
-        count += 1
+        count +=1 
     
     return count
