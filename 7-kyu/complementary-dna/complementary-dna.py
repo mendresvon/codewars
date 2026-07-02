60422554759510a0214cb8f2ef97aebbe0796b12
Von Breznev Antido Mendres <mendresvon@gmail.com>
2026-07-02 17:34:25 +0800

    Update solution - CodeHub

--- 7-kyu/complementary-dna/complementary-dna.py
+++ 7-kyu/complementary-dna/complementary-dna.py
@@ -1,14 +1,9 @@
 def DNA_strand(dna):
-    dna = list(dna)
+    pairs = {
+        'A': 'T',
+        'T': 'A',
+        'G': 'C',
+        'C': 'G'
+    }
     
-    for i in range(len(dna)):
-        if dna[i] == 'A':
-            dna[i] = 'T'
-        elif dna[i] == 'T':
-            dna[i] = 'A'
-        elif dna[i] == 'C':
-            dna[i] = 'G'
-        elif dna[i] == 'G':
-            dna[i] = 'C'
-    
-    return ''.join(dna)
+    return ''.join(pairs[char] for char in dna)
