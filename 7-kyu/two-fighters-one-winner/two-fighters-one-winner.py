ddfd1e9550682ef0ebceec8892ef286b87c48dda
Von Breznev Antido Mendres <mendresvon@gmail.com>
2026-07-03 11:28:57 +0800

    Update solution - CodeHub

--- 7-kyu/two-fighters-one-winner/two-fighters-one-winner.py
+++ 7-kyu/two-fighters-one-winner/two-fighters-one-winner.py
@@ -1,12 +1,11 @@
 def declare_winner(fighter1, fighter2, first_attacker):
     # Code your solution here
-    attacker = fighter1 if fighter1.name == first_attacker else fighter2
-    defender = fighter2 if fighter1.name == first_attacker else fighter1
+    attacker = fighter1 if first_attacker == fighter1.name else fighter2
+    defender = fighter2 if first_attacker == fighter1.name else fighter1
     
     while attacker.health > 0:
         defender.health -= attacker.damage_per_attack
         if defender.health <= 0:
             return attacker.name
-        
-        attacker, defender = defender, attacker
+        defender, attacker = attacker, defender
     
