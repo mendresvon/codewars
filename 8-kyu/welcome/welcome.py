42fc5eeedd8e4a0054ae5b939a579cda168bbb1c
Von Breznev Antido Mendres <mendresvon@gmail.com>
2026-07-16 21:59:25 +0800

    Add solution - CodeHub

---
+++ 8-kyu/welcome/welcome.py
@@ -0,0 +1,25 @@
+def greet(language):
+    database = {
+        "english": "Welcome",
+        "czech": "Vitejte",
+        "danish": "Velkomst",
+        "dutch": "Welkom",
+        "estonian": "Tere tulemast",
+        "finnish": "Tervetuloa",
+        "flemish": "Welgekomen",
+        "french": "Bienvenue",
+        "german": "Willkommen",
+        "irish": "Failte",
+        "italian": "Benvenuto",
+        "latvian": "Gaidits",
+        "lithuanian": "Laukiamas",
+        "polish": "Witamy",
+        "spanish": "Bienvenido",
+        "swedish": "Valkommen",
+        "welsh": "Croeso"
+    }
+    
+    if language not in database:
+        language = "english"
+        
+    return database[language]
