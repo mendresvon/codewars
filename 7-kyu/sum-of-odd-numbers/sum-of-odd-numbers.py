71985e40f1297f07679ea55024dd0a918ca78821
Von Breznev Antido Mendres <mendresvon@gmail.com>
2026-07-21 16:06:10 +0800

    Update solution - CodeHub

--- 7-kyu/sum-of-odd-numbers/sum-of-odd-numbers.py
+++ 7-kyu/sum-of-odd-numbers/sum-of-odd-numbers.py
@@ -1,2 +1,9 @@
 def row_sum_odd_numbers(n):
-    return n**3
+    # 1: 1
+    # 2: 3
+    # 3: 7
+    # 4: 13
+    # 1 + n * (n-1)
+    start = 1 + n * (n-1) # start of the row
+    
+    return sum(range(start, start + 2 * (n-1) + 1, 2))
