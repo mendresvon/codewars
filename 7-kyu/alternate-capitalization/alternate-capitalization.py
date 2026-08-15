31f1ce5591a5c6a1f9efd84f7a3819129a380c84
Von Breznev Antido Mendres <mendresvon@gmail.com>
2026-08-15 20:16:39 +0800

    Update solution - CodeHub

--- 7-kyu/alternate-capitalization/alternate-capitalization.py
+++ 7-kyu/alternate-capitalization/alternate-capitalization.py
@@ -1,2 +1,11 @@
 def capitalize(s):
-    return [''.join(s[i].upper() if i%2==0 else s[i] for i in range(len(s))), ''.join(s[i].upper() if i%2!=0 else s[i] for i in range(len(s)))]
+    res = [[], []]
+    for i, c in enumerate(s):
+        if i % 2 == 0:
+            res[0].append(c.upper())
+            res[1].append(c)
+        else:
+            res[0].append(c)
+            res[1].append(c.upper())
+    
+    return [''.join(res[0]), ''.join(res[1])]
