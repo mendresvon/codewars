fb9b0d7e24a0007f951dcec0785bfcbebc29a2b9
Von Breznev Antido Mendres <mendresvon@gmail.com>
2026-08-30 11:04:59 +0800

    Add solution - CodeHub

---
+++ 6-kyu/equal-sides-of-an-array/equal-sides-of-an-array.py
@@ -0,0 +1,12 @@
+def find_even_index(arr):
+    l_sum = 0
+    r_sum = sum(arr)
+    
+    for i in range(len(arr)):
+        r_sum -= arr[i]
+        if l_sum == r_sum:
+            return i
+        l_sum += arr[i]
+        
+    
+    return -1
