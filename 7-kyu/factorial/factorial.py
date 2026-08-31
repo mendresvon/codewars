b96576785a7f505cf81d00a9bca88801aeaf4893
Von Breznev Antido Mendres <mendresvon@gmail.com>
2026-08-31 21:30:33 +0800

    Update solution - CodeHub

--- 7-kyu/factorial/factorial.py
+++ 7-kyu/factorial/factorial.py
@@ -1,6 +1,7 @@
 def factorial(n):
-    if 0 > n or n > 12:
-        raise ValueError("Hi!")
+    if n < 0 or n > 12:
+        raise ValueError
+    
     if n == 0:
         return 1
     return n * factorial(n-1)
