9aed69706badfae66cacc16973f22e1a1c0331a6
Von Breznev Antido Mendres <mendresvon@gmail.com>
2026-09-24 19:03:15 +0800

    Update solution - CodeHub

--- 7-kyu/sum-of-a-sequence/sum-of-a-sequence.py
+++ 7-kyu/sum-of-a-sequence/sum-of-a-sequence.py
@@ -2,4 +2,10 @@ def sequence_sum(begin_number, end_number, step):
     if begin_number > end_number:
         return 0
     
-    return sum(range(begin_number, end_number+1, step))
+    summ = 0
+    
+    while begin_number <= end_number:
+        summ += begin_number
+        begin_number += step
+    
+    return summ
